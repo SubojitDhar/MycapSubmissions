b489e0cab1ec38eabd6b47b48615946c138f13a1
SubojitDhar <71097341+SubojitDhar@users.noreply.github.com>
2021-04-11 15:32:20 +0530

    Delete file.py

--- file.py
+++
@@ -1,6 +0,0 @@
-file_name=input("enter the name ")
-leng=int(len(file_name))
-if (leng-(file_name.rfind('py'))==2):
-    print("File extension is python")
-else:
-    print("Other file extension found")
