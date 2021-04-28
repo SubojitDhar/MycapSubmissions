50f21e9517b38d1ddafc94951f53e4a1c9aa667d
SubojitDhar <71097341+SubojitDhar@users.noreply.github.com>
2021-04-28 11:08:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,153 +1,39 @@
-#Students Administration System
-import csv
-import os
+import pandas
+import requests
 
-def write_into_csv(info_list):# Filling Details
-    with open('students_info_file.csv','a', newline='') as csv_file:
-        writer=csv.writer(csv_file)
-        if csv_file.tell() == 0:
-            writer.writerow(['Name', 'Roll', 'Class', 'Age', 'Email-ID', 'Registration-ID'])
-        with open('students_info_file.csv', 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            for i in reader:
-                if i[5] == info_list[5]:
-                    print('Record with Same Registration Id already Present\n Pls enter non-duplicate details')
-                    ask_details()
-            writer.writerow(info_list)
+from bs4 import BeautifulSoup
 
-def ask_details():# Asking Details
-        count=1
-        count_2=0
-        while(True):
-            details=['Name','Roll','Class','Age','Email-ID','Registration-ID',]
-            print('Enter the details for student #{} : \n'.format(count))
-            details[0] = input('1. Name     : ')
-            details[1] = input('2. Roll     : ')
-            details[2] = input('3. Class    : ')
-            details[3] = int(input('4. Age      : '))
-            details[4] = input('5. Email-ID : ')
-            details[5] = input('6. Reg-ID    : ')
-            if count_2 != 0:
-                continue
-            print('\nAre the details provided corrected: \n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} '
-                  '\n5. Email-ID: {}\n6. Registration-ID: {}'
-                  .format(details[0],details[1],details[2],details[3],details[4],details[5]))
-            choice_1=input('Enter yes or no : ')
-            if choice_1 == 'no':
-                print('\n Enter correct details \n')
-                continue
-            write_into_csv(details)
-            choice_2=input('Do you want to enter the details of any other students \nEnter yes or no : ')
-            if choice_2 == 'no':
-                break
-            else:
-                count+=1
-
-def ser_details():# Searching  Details
-    with open('students_info_file.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        count = 0
-        choice=int(input('Search by :\n1. Name\n2. Roll\n3. Class\n4. Age \n5.Email-ID\n6.Registration-ID\n'))
-        if choice == 1:
-            Name = input('Enter the Name : ')
-            for i in reader:
-                if Name == i[0]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-        elif choice == 2:
-            Roll = input('Enter the Roll : ')
-            for i in reader:
-                if Roll == i[1]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-                    break
-        elif choice == 3:
-            Class = input('Enter the Class : ')
-            for i in reader:
-                if Class == i[2]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-        elif choice == 4:
-            Age = int(input('Enter the Age : '))
-            for i in reader:
-                if Age == i[3]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-        elif choice == 5:
-            Email = input('Enter the Email-ID : ')
-            for i in reader:
-                if Email == i[4]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-                    break
-        elif choice == 6:
-            Reg = input('Enter the Registration-ID : ')
-            for i in reader:
-                if Reg == i[5]:
-                    print('\n\nStudents details found')
-                    print('\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} \n5.Email-ID: {}'
-                          .format(i[0], i[1], i[2], i[3], i[4]))
-                    count += 1
-                    break
-        else:
-            print('Entered wrong values !!!')
-        if count == 0:
-            print(' No Record found ')
-
-def prt_details():# Printing Details
-    with open('students_info_file.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        count=1
-        for i in reader:
-            if count>1 :
-                print('Student #{}'.format(count-1),'\n1. Name   : {} \n2. Roll   : {} \n3. Class  : {} \n4. Age    : {} '
-                                                  '\n5.Email-ID: {}\n6.Registration-ID: {}'
-                    .format(i[0], i[1], i[2], i[3], i[4],i[5]),'\n\n')
-            count+= 1
-
-def del_details():# Deleting Details
-    with open('students_info_file.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        with open('students_info.csv', 'a', newline='') as csv_file_2:
-            writer = csv.writer(csv_file_2)
-            Roll=input('Enter the Roll no. : ')
-            for i in reader:
-                if Roll != i[1]:
-                    writer.writerow(i)
-    csv_file.close()
-    os.remove('students_info_file.csv')
-    os.rename('students_info.csv','students_info_file.csv')
-
-if __name__ == '__main__':
-    print('\nWelcome to Student Administration System :\n')
-    while(True):
-        print('\n1.Enter Students Details.'
-              '\n2.Delete Students Details\n3.Print Students details\n4.Search Students details\n\nEnter your choice (1/2/3/4) :\n')
-        choice=int(input())
-        if choice == 1:
-            ask_details()
-        elif choice == 2:
-            del_details()
-        elif choice == 3:
-            prt_details()
-        elif choice == 4:
-            ser_details()
-        else:
-            print('Entered wrong values !!!')
-        choice_3 = input('\nDo you want to perform any other operations?  \nEnter yes or no : ')
-        if choice_3 == 'no':
-            break
-        elif choice_3 == 'yes':
-            continue
-        else:
-            print('Entered wrong values !!!')
+oyo_url = "https://www.oyorooms.com/search?location=Kolkata%2C%20West%20Bengal%2C%20India&city=Kolkata&searchType=city&checkin=24%2F04%2F2021&checkout=25%2F04%2F2021&roomConfig%5B%5D=1&guests=1&rooms=1&filters%5Bcity_id%5D=14"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36'}
+req = requests.get(oyo_url, headers=headers)
+content = req.content
+soup = BeautifulSoup(content, "html.parser")
+all_hotels = soup.find_all("div", {"class": "oyo-row"})
+name = soup.find_all("h3", {"itemprop": "name"})
+address = soup.find_all("div", {"itemprop": "address"})
+price = soup.find_all("span", {"class": "listingPrice__finalPrice"})
+rating = soup.find_all("span", {"class": "hotelRating__ratingSummary"})
+hotel_name = []
+hotel_address = []
+hotel_rating = []
+hotel_price = []
+for i in name:
+    hotel_name.append(i.text)
+for i in address:
+    hotel_address.append(i.text)
+for i in rating:
+    hotel_rating.append(i.text)
+for i in price:
+    hotel_price.append(i.text)
+fake = ["","","",""]
+scrap_into_csv=[]
+scrap_into_csv.append(fake)
+scrap_into_csv[0] = ["name", "address", "rating", "price"]
+for i in range(len(hotel_name)):
+    scrap_into_csv.append(fake)
+    scrap_into_csv[i+1]=[hotel_name[i],hotel_address[i],hotel_rating[i],hotel_price[i]]
+for i in scrap_into_csv:
+    print(i)
+data_Frame = pandas.DataFrame(scrap_into_csv)
+data_Frame.to_csv("Oyo.csv")
